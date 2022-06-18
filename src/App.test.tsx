8f3ac7dcbe42c9bfc78cff4cd963e5c1ca6714a2
Serkan KONAKCI <serkankonakci@gmail.com>
2022-06-18 12:01:01 +0300

    fix: add browser route to app.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import { HomeScreen } from 'src/screens/home.screen';
 
 test('renders learn react link', async () => {
-  render(<HomeScreen />);
+  render(
+    <BrowserRouter>
+      <HomeScreen />
+    </BrowserRouter>,
+  );
   const linkElement = await screen.getByRole('button', { name: /edit/i });
 
   expect(linkElement).toBeInTheDocument();
