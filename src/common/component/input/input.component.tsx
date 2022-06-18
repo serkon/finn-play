32bf76d0a302b56618dab7cd5aef2523326ddb44
Serkan KONAKCI <serkankonakci@gmail.com>
2022-06-18 19:35:00 +0300

    refactor: update components and add header component

--- src/common/component/input/input.component.tsx
+++ src/common/component/input/input.component.tsx
@@ -2,20 +2,21 @@ import React from 'react';
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string;
-  icon?: string;
+  iconLeft?: string;
+  iconRight?: string;
 }
 
-export const Input = ({ label, icon, ...rest }: InputProps) => {
+export const Input = ({ label, iconLeft, iconRight, ...rest }: InputProps) => {
   const ref = React.useRef<HTMLInputElement>(null);
   const set = () => {
     const type = (ref.current as HTMLInputElement).type === 'password' ? 'text' : 'password';
     (ref.current as HTMLInputElement).type = type;
   };
   return (
-    <label className={`input-component${icon ? ' with-icon' : ''}`}>
+    <label className={`input-component${iconLeft ? ' icon-left with-icon' : iconRight ? ' icon-right with-icon' : ''}`}>
       <input type="text" {...rest} required ref={ref} />
       <span>{label}</span>
-      {icon && <i className={`icon icon-${icon}`} onClick={set} />}
+      {(iconLeft || iconRight) && <i className={`icon icon-${iconLeft || iconRight}`} onClick={set} />}
     </label>
   );
 };
