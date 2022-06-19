9a121404d3008c35001eb014c37ebe3f49b8a40b
Serkan KONAKCI <serkankonakci@gmail.com>
2022-06-19 14:29:15 +0300

    feat: add ref object to input for get value out of component

--- src/common/component/input/input.component.tsx
+++ src/common/component/input/input.component.tsx
@@ -4,8 +4,24 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string;
   iconLeft?: string;
   iconRight?: string;
+  ref?: React.MutableRefObject<HTMLInputElement>;
 }
 
+export const Input = React.forwardRef<HTMLInputElement, InputProps>(({ label, iconLeft, iconRight, ...rest }, forwardedRef) => {
+  const set = () => {
+    const ref = forwardedRef && typeof forwardedRef !== 'function' && forwardedRef?.current;
+    (ref as HTMLInputElement).type = ref && ref.type === 'password' ? 'text' : 'password';
+  };
+  return (
+    <label className={`input-component${iconLeft ? ' icon-left with-icon' : iconRight ? ' icon-right with-icon' : ''}`}>
+      <input type="text" {...rest} required ref={forwardedRef} />
+      <span>{label}</span>
+      {(iconLeft || iconRight) && <i className={`icon icon-${iconLeft || iconRight}`} onClick={set} />}
+    </label>
+  );
+});
+
+/*
 export const Input = ({ label, iconLeft, iconRight, ...rest }: InputProps) => {
   const ref = React.useRef<HTMLInputElement>(null);
   const set = () => {
@@ -20,3 +36,4 @@ export const Input = ({ label, iconLeft, iconRight, ...rest }: InputProps) => {
     </label>
   );
 };
+*/
