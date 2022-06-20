77adfd2bd12b4e191e4c95ded45fe6517c275918
Serkan KONAKCI <serkankonakci@gmail.com>
2022-06-20 20:02:17 +0300

    feat: add redux mock test support

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,20 @@
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { HomeScreen } from 'src/screens/home/home.screen';
+import configureStore from 'redux-mock-store';
 
 test('renders learn react link', async () => {
+  const initialState = { output: 10 };
+  const mockStore = configureStore();
+  const store = mockStore(initialState);
+
   render(
-    <BrowserRouter>
-      <HomeScreen />
-    </BrowserRouter>,
+    <Provider store={store}>
+      <BrowserRouter>
+        <HomeScreen />
+      </BrowserRouter>
+    </Provider>,
   );
   const linkElement = await screen.getByRole('button', { name: /edit/i });
 
